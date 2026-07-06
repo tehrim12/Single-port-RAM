db8676e2a20aa45e0ba9f4f80e5de261342b39f7
tehrim12 <stehrim26@gmail.com>
2026-07-07 00:09:25 +0530

    first commit

---
+++ tb/ram_pkg.sv
@@ -0,0 +1,11 @@
+package ram_pkg;
+  `include "defines.sv"
+  `include "ram_transaction.sv"
+  `include "ram_generator.sv"
+  `include "ram_driver.sv"
+  `include "ram_monitor.sv"
+  `include "ram_reference_model.sv"
+  `include "ram_scoreboard.sv"
+  `include "ram_environment.sv"
+  `include "ram_test.sv"       // If you have a test class, include it last
+endpackage
